8dca8fb6ee05247d5ef128ebbe41f9f8837e35e6
Max Beneke <max.beneke@gmail.com>
2020-12-29 15:58:05 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -58,9 +58,10 @@ def runner
   
   welcome
   initial_round
-  
+  card_total = initial_round
   until card_total > 21 do 
     hit?(card_total)
+    display_card_total(card_total)
   end
   end_game(card_total)
   
